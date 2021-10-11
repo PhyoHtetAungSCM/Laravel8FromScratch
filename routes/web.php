e08c4a98bc1feb2054f9a92d684e4a321c059ca6
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-11 23:27:18 +0630

    Section 5 | Integrate the Design

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -32,12 +33,15 @@ Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         // 'posts' => $category->posts->load(['category', 'author']),
         'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         // 'posts' => $author->posts->load(['category', 'author']),
         'posts' => $author->posts,
+        'categories' => Category::all(),
     ]);
 });
