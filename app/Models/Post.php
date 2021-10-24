069f6c5dc74c2689bca9ba77127cc330341e2148
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-24 10:44:48 +0630

    Sec 7, Epi 39 | Advanced Eloquent Query Constraints

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,8 +23,19 @@ class Post extends Model
     {
         $query->when($filters['search'] ?? false, fn($query, $search) =>
             $query
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('excerpt', 'like', '%' . request('search') . '%'));
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('excerpt', 'like', '%' . $search . '%')
+        )
+            ->when($filters['category'] ?? false, fn($query, $category) =>
+                $query->whereHas('category', fn($query) =>
+                    $query->where('slug', $category)
+                )
+                // $query->whereExists(fn($query) =>
+                //     $query->from('categories')
+                //         ->whereColumn('categories.id', 'posts.category_id')
+                //         ->where('categories.slug', $category)
+                // )
+            );
     }
 
     public function category()
