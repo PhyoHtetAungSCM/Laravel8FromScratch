f4200d4f04ee3cd7e7b5afd4cb9942872974fb53
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-24 14:21:20 +0630

    Sec 9, Epi 49 | Login and Logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -20,7 +21,9 @@ class RegisterController extends Controller
             'password' => 'required|min:6|max:255',
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        Auth::login($user);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
