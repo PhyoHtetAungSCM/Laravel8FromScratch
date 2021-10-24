2080f73f59797963a455e6f31fcc8c46c4bd5bb3
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-24 09:55:37 +0630

    Section 6, Epi 37 | Search (The Messy Way)

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,16 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts = $posts->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('excerpt', 'like', '%' . request('search') . '%');
+    }
+
     return view('posts', [
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all(),
     ]);
 })->name('home');
