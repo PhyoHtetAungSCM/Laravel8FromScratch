913297be00300ac0f89364e3b1492725c1e078a2
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-24 10:19:23 +0630

    Section 6, Epi 38 | Search (The Cleaner Way)

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts = $posts->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('excerpt', 'like', '%' . request('search') . '%');
-    }
-
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all(),
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post,
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
