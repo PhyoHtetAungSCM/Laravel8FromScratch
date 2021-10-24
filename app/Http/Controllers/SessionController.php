396cd3c0d58c3a8fd5265486934bf6c0b0e47553
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-24 14:38:37 +0630

    Sec 9, Epi 50 | Build the Log In Page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // attempt to authenticate and log in the user
+        // based  on the provided credentials
+        if (Auth::attempt($attributes)) {
+            session()->regenerate(); //session fixation
+
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.',
+        ]);
+    }
+
     public function destroy()
     {
         Auth::logout();
