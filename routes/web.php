931410c2686a0b02d2bf73ce9d12b105f6d37113
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-27 07:22:25 +0630

    Sec 11, Epi 58 | Mailchimp API Tinkering

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ Route::post('register', [RegisterController::class, 'store'])->middleware(('gues
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5',
+    ]);
+
+    $response = $mailchimp->lists->addListMember('8b573a1efb', [
+        'email_address' => 'scm.phyohtetaung@gmail.com',
+        'status' => 'subscribed',
+    ]);
+
+    ddd($response);
+});
