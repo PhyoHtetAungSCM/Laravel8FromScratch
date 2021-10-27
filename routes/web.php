e96416658aad4919d5bd5e9dfe3c9287a977ef23
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-10-27 07:51:40 +0630

    Sec 11, Epi 60 | Extract a Newsletter Service

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -11,33 +12,11 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
+Route::post('newsletter', NewsletterController::class);
+
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware(('guest'));
 
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
-
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required|email']);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us5',
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember('8b573a1efb', [
-            'email_address' => request('email'),
-            'status' => 'subscribed',
-        ]);
-    } catch (\Exception$e) {
-        \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.',
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
